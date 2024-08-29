846b94319aca44d486e300109d41acf3f2826baf
alxkt <alexkturnbull@gmail.com>
2024-08-29 15:26:10 -0700

    added a sorted word count method and sub methods

---
+++ main.py
@@ -0,0 +1,40 @@
+def main():
+  path_to_book = "books/frankenstein.txt"
+  text = get_book_text(path_to_book)
+  test_string = "This is shorter so that this is not crazy. Easier to test!"
+  letters = letter_frequency(text)
+  letter_report(letters)
+  return
+
+def get_book_text(path):
+  with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+  return file_contents
+
+def string_to_words(string):
+  words = string.split()
+  return words
+
+def letter_frequency(string):
+  letters = {} 
+  lowercase_string = string.lower()
+  for letter in lowercase_string:
+    if letter.isalpha():
+      if letter in letters:
+        letters[letter] += 1
+      else:
+        letters[letter] = 1
+  return letters
+
+def sort_on(dict):
+  return dict["number"]
+
+def letter_report(letters):
+  sorted_list = []
+  for letter in letters:
+    sorted_list.append({"name": letter, "number": letters[letter]})
+  sorted_list.sort(reverse=True, key=sort_on)
+  for letter in sorted_list:
+    print(f"{letter['name']} appears {letter['number']} times")
+
+main()
